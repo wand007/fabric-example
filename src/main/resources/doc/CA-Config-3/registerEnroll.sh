d7f804befb63f2ee97cf833d01a2c9590b083082
咚咚锵 <2233172365@qq.com>
2020-09-25 22:23:35 +0800

    add

--- src/main/resources/doc/CA-Config-3/registerEnroll.sh
+++ src/main/resources/doc/CA-Config-3/registerEnroll.sh
@@ -1,22 +1,25 @@
-工作目录,除非特殊说明，一般命令的执行都是在工作目录进行。
-cd $GOPATH/src/github.com/hyperledger/fabric-samples/first
+## 工作目录,除非特殊说明，一般命令的执行都是在工作目录进行。
+## cd $GOPATH/src/github.com/hyperledger/fabric-samples/first
 
-首次运行清除docker影响
+## 首次运行清除docker影响
 
-docker-compose -f docker-compose-cli-ca.yaml  down --volumes --remove-orphans
+## docker-compose -f docker-compose-ca.yaml  down --volumes --remove-orphans
+
+## docker ps -a|awk '{print $1}'|xargs -i docker stop {}
+## docker ps -a|awk '{print $1}'|xargs -i docker rm {}
 
 export FABRIC_CA_BASE_HOME=$GOPATH/src/github.com/hyperledger/fabric-samples/first-ca
 
-启动CA服务
-docker-compose -f docker-compose-cli-ca.yaml up -d 2>&1
+## 启动CA服务
+docker-compose -f docker-compose-ca.yaml up -d 2>&1
 
 
-登陆
+## 登陆
 export FABRIC_CA_CLIENT_TLS_CERTFILES=$FABRIC_CA_BASE_HOME/crypto-config/tls-ca/crypto/ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=$FABRIC_CA_BASE_HOME/crypto-config/tls-ca/admin
 fabric-ca-client enroll -d -u https://tls-ca-admin:tls-ca-adminpw@0.0.0.0:7052
 
-注册
+## 注册
 # 为各个组件(组件指的是 peer,order和管理员)注册TLS证书,仅仅是注册了身份,并没有获取到证书;
 fabric-ca-client register -d --id.name peer1-org1 --id.secret peer1PW --id.type peer -u https://0.0.0.0:7052
 fabric-ca-client register -d --id.name peer2-org1 --id.secret peer2PW --id.type peer -u https://0.0.0.0:7052
@@ -33,12 +36,12 @@ fabric-ca-client register -d --id.name orderer5-org0 --id.secret ordererPW --id.
 
 
 
-登录
+## 登录
 export FABRIC_CA_CLIENT_TLS_CERTFILES=$FABRIC_CA_BASE_HOME/crypto-config/org0/ca/crypto/ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=$FABRIC_CA_BASE_HOME/crypto-config/org0/ca/admin
 fabric-ca-client enroll -d -u https://org0-admin:org0-adminpw@0.0.0.0:7053
 
-注册
+## 注册
 fabric-ca-client register -d --id.name orderer1-org0 --id.secret ordererpw --id.type orderer --id.attrs '"hf.Registrar.Roles=orderer"' -u https://0.0.0.0:7053
 fabric-ca-client register -d --id.name orderer2-org0 --id.secret ordererpw --id.type orderer --id.attrs '"hf.Registrar.Roles=orderer"' -u https://0.0.0.0:7053
 fabric-ca-client register -d --id.name orderer3-org0 --id.secret ordererpw --id.type orderer --id.attrs '"hf.Registrar.Roles=orderer"' -u https://0.0.0.0:7053
@@ -49,13 +52,12 @@ fabric-ca-client register -d --id.name admin-org0 --id.secret org0adminpw --id.t
 
 
 
-登录
-
+## 登录
 export FABRIC_CA_CLIENT_TLS_CERTFILES=$FABRIC_CA_BASE_HOME/crypto-config/org1/ca/crypto/ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=$FABRIC_CA_BASE_HOME/crypto-config/org1/ca/admin
 fabric-ca-client enroll -d -u https://org1-admin:org1-adminpw@0.0.0.0:7054
 
-注册
+## 注册
 fabric-ca-client register -d --id.name peer1-org1 --id.secret peer1PW --id.type peer -u https://0.0.0.0:7054
 fabric-ca-client register -d --id.name peer2-org1 --id.secret peer2PW --id.type peer -u https://0.0.0.0:7054
 fabric-ca-client register -d --id.name admin-org1 --id.secret org1AdminPW --id.type admin -u https://0.0.0.0:7054
@@ -64,20 +66,20 @@ fabric-ca-client register -d --id.name admin-org1 --id.secret org1AdminPW --id.t
 
 
 
-登录
+## 登录
 export FABRIC_CA_CLIENT_TLS_CERTFILES=$FABRIC_CA_BASE_HOME/crypto-config/org2/ca/crypto/ca-cert.pem
 export FABRIC_CA_CLIENT_HOME=$FABRIC_CA_BASE_HOME/crypto-config/org2/ca/admin
 fabric-ca-client enroll -d -u https://org2-admin:org2-adminpw@0.0.0.0:7055
 
 
-注册
+## 注册
 fabric-ca-client register -d --id.name peer1-org2 --id.secret peer1PW --id.type peer -u https://0.0.0.0:7055
 fabric-ca-client register -d --id.name peer2-org2 --id.secret peer2PW --id.type peer -u https://0.0.0.0:7055
 fabric-ca-client register -d --id.name admin-org2 --id.secret org2AdminPW --id.type admin -u https://0.0.0.0:7055
 
 
 
-通过客户端为所有节点生成msp证书和tls证书
+## 通过客户端为所有节点生成msp证书和tls证书
 
 
 export FABRIC_CA_CLIENT_HOME=$FABRIC_CA_BASE_HOME/crypto-config/org1/peer1
@@ -182,7 +184,7 @@ mkdir -p $FABRIC_CA_BASE_HOME/crypto-config/org2/peer2/msp/admincerts
 cp $FABRIC_CA_BASE_HOME/crypto-config/org2/admin/msp/signcerts/cert.pem $FABRIC_CA_BASE_HOME/crypto-config/org2/peer2/msp/admincerts/org2-admin-cert.pem
 
 
-org0内的orderer节点制作证书
+## org0内的orderer节点制作证书
 
 mkdir -p $FABRIC_CA_BASE_HOME/crypto-config/org0/orderers
 
@@ -308,7 +310,7 @@ cp ./org2-config/config.yaml ./crypto-config/org2/peer1/msp/
 cp ./org2-config/config.yaml ./crypto-config/org2/peer2/msp/
 
 
-org0的admin证书
+## org0的admin证书
 
 mkdir -p $FABRIC_CA_BASE_HOME/crypto-config/configtx/org0
 cp -r $FABRIC_CA_BASE_HOME/crypto-config/org0/admin/msp $FABRIC_CA_BASE_HOME/crypto-config/configtx/org0
@@ -316,14 +318,14 @@ cp -r $FABRIC_CA_BASE_HOME/crypto-config/org0/admin/msp $FABRIC_CA_BASE_HOME/cry
 mkdir $FABRIC_CA_BASE_HOME/crypto-config/configtx/org0/msp/tlscacerts
 cp  $FABRIC_CA_BASE_HOME/crypto-config/org0/orderers/orderer1-org0/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem  $FABRIC_CA_BASE_HOME/crypto-config/configtx/org0/msp/tlscacerts
 
-org1的admin证书
+## org1的admin证书
 mkdir -p $FABRIC_CA_BASE_HOME/crypto-config/configtx/org1
 cp -r $FABRIC_CA_BASE_HOME/crypto-config/org1/admin/msp $FABRIC_CA_BASE_HOME/crypto-config/configtx/org1/
 
 mkdir -p $FABRIC_CA_BASE_HOME/crypto-config/configtx/org1/msp/tlscacerts
 cp $FABRIC_CA_BASE_HOME/crypto-config/org1/admin/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem $FABRIC_CA_BASE_HOME/crypto-config/configtx/org1/tlscacerts
 
-org2的admin证书
+## org2的admin证书
 mkdir -p $FABRIC_CA_BASE_HOME/crypto-config/configtx/org2
 cp -r $FABRIC_CA_BASE_HOME/crypto-config/org2/admin/msp $FABRIC_CA_BASE_HOME/crypto-config/configtx/org2/
 
@@ -334,10 +336,10 @@ cp $FABRIC_CA_BASE_HOME/crypto-config/org2/admin/tls-msp/tlscacerts/tls-0-0-0-0-
 
 configtxgen -profile SampleMultiNodeEtcdRaft -channelID byfn-sys-channel -outputBlock ./channel-artifacts/genesis.block
 configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID mychannel -asOrg Org1MSP
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID mychannel -asOrg Org2MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID mychannel -asOrg org1MSP
+configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID mychannel -asOrg org2MSP
 
-docker-compose -f docker-compose-orderer1.yaml up
+## docker-compose -f docker-compose-orderer1.yaml up
 
 
 
