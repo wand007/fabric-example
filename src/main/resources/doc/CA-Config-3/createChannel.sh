f5806058a7bc9b03a933685ce7a26a2e46130fa6
李东东 <lidongdong@gxcards.com>
2020-09-28 19:31:10 +0800

    add channel

--- src/main/resources/doc/CA-Config-3/createChannel.sh
+++ src/main/resources/doc/CA-Config-3/createChannel.sh
@@ -9,22 +9,41 @@ peer channel create -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-art
 
 mv mychannel.block /usr/local/channel-artifacts/
 
+exit
+
+docker exec -it cli.peer1.org1 bash
 # 加入通道
 peer channel join -b /usr/local/channel-artifacts/mychannel.block
 
+exit
 
-
-docker exec -it cli.peer1.org2 bash
-
+docker exec -it cli.peer2.org2 bash
+# 加入通道
 peer channel join -b /usr/local/channel-artifacts/mychannel.block
 
+exit
+
+docker exec -it cli.peer1.org2 bash
+# 加入通道
+peer channel join -b /usr/local/channel-artifacts/mychannel.block
+
+exit
+
+docker exec -it cli.peer2.org2 bash
+# 加入通道
+peer channel join -b /usr/local/channel-artifacts/mychannel.block
+
+exit
 
 # 更新锚节点
 docker exec -it cli.peer1.org1 bash
 
 peer channel update -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-artifacts/Org1MSPanchors.tx --tls true --cafile $ORDERER_CA
 
+exit
 
 docker exec -it cli.peer1.org2 bash
 
 peer channel update -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-artifacts/Org2MSPanchors.tx --tls true --cafile $ORDERER_CA
+
+exit
