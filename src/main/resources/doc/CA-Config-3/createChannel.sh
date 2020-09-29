75a770808efb649d8649a4d57e9732e6654f2b90
李东东 <lidongdong@gxcards.com>
2020-09-29 16:37:33 +0800

    add peer channel join

--- src/main/resources/doc/CA-Config-3/createChannel.sh
+++ src/main/resources/doc/CA-Config-3/createChannel.sh
@@ -3,47 +3,45 @@
 docker-compose -f docker-compose-cli-peers.yaml up  -d 2>&1
 
 
-docker exec -it cli.peer1.org1 bash
+docker exec -it cli-org1-peer1 bash
 # 创建通道
-peer channel create -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-artifacts/channel.tx --tls true --cafile $ORDERER_CA
-
-mv mychannel.block /usr/local/channel-artifacts/
+peer channel create -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-artifacts/channel.tx --outputBlock /usr/local/channel-artifacts/mychannel.block --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE
 
 exit
 
-docker exec -it cli.peer1.org1 bash
+docker exec -it cli-org1-peer1 bash
 # 加入通道
 peer channel join -b /usr/local/channel-artifacts/mychannel.block
 
 exit
 
-docker exec -it cli.peer2.org2 bash
+docker exec -it cli-org1-peer2 bash
 # 加入通道
 peer channel join -b /usr/local/channel-artifacts/mychannel.block
 
 exit
 
-docker exec -it cli.peer1.org2 bash
+docker exec -it cli-org2-peer1 bash
 # 加入通道
 peer channel join -b /usr/local/channel-artifacts/mychannel.block
 
 exit
 
-docker exec -it cli.peer2.org2 bash
+docker exec -it cli-org2-peer2 bash
 # 加入通道
 peer channel join -b /usr/local/channel-artifacts/mychannel.block
 
 exit
 
 # 更新锚节点
-docker exec -it cli.peer1.org1 bash
+docker exec -it cli-org1-peer1 bash
 
-peer channel update -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-artifacts/Org1MSPanchors.tx --tls true --cafile $ORDERER_CA
+peer channel update -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-artifacts/Org1MSPanchors.tx --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE
 
 exit
 
-docker exec -it cli.peer1.org2 bash
+docker exec -it cli-org2-peer1 bash
 
-peer channel update -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-artifacts/Org2MSPanchors.tx --tls true --cafile $ORDERER_CA
+peer channel update -o orderer1-org0:7050 -c mychannel -f /usr/local/channel-artifacts/Org2MSPanchors.tx --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE
 
 exit
