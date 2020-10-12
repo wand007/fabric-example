bfd308d33219c0e689eafc43ad0cbcc4807c70f6
李东东 <lidongdong@gxcards.com>
2020-10-12 19:07:48 +0800

    名词规则定义

--- src/main/resources/doc/CA-Config-1/createChannelPrivateData.sh
+++ src/main/resources/doc/CA-Config-1/createChannelPrivateData.sh
@@ -59,7 +59,7 @@ docker exec -it cli-org1-peer0 bash
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
 
-pushd /opt/gopath/src/github.com/hyperledger/chaincode/marbles02_private/go/
+pushd /opt/gopath/src/github.com/hyperledger/chaincode/chaincode/marbles02_private/go/
 GO111MODULE=on go mod vendor
 popd
 
@@ -77,10 +77,10 @@ export CC_PACKAGE_ID=marbles02_private_1:a7fe3a7a4e0124b9a9b86960dac9a28464d1dbd
 peer lifecycle chaincode queryinstalled
 
 # 链码认证 根据设置的链码审批规则，只需要当前组织中的任意一个节点审批通过即可
-peer lifecycle chaincode approveformyorg --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE --channelID $CHANNEL_NAME --name marbles02_private --version 1.2 --sequence 1 --init-required --package-id $CC_PACKAGE_ID --collections-config /opt/gopath/src/github.com/hyperledger/chaincode/marbles02_private/collections_config.json --waitForEvent
+peer lifecycle chaincode approveformyorg --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE --channelID $CHANNEL_NAME --name marbles02_private --version 1 --init-required --package-id $CC_PACKAGE_ID --sequence 1 --waitForEvent
 
 # 查看链码认证结果 此时只有Org1MSP审核通过了
-peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_NAME --name marbles02_private --version 1.2 --sequence 1 --output json --init-required
+peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_NAME --name marbles02_private --version 1 --sequence 1 --output json --init-required
 
 exit
 
@@ -99,9 +99,7 @@ export CC_PACKAGE_ID=marbles02_private_1:a7fe3a7a4e0124b9a9b86960dac9a28464d1dbd
 peer lifecycle chaincode queryinstalled
 
 # 链码认证 根据设置的链码审批规则，只需要当前组织中的任意一个节点审批通过即可
-peer lifecycle chaincode approveformyorg --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE --channelID $CHANNEL_NAME --name marbles02_private --version 1 --collections-config /opt/gopath/src/github.com/hyperledger/chaincode/marbles02_private/collections_config.json  --signature-policy "OR('Org1MSP.member','Org2MSP.member')"  --init-required --package-id $CC_PACKAGE_ID --sequence 1 --waitForEvent
-
-peer lifecycle chaincode approveformyorg --channelID $CHANNEL_NAME --name marbles02_private --version 1.0 --collections-config /opt/gopath/src/github.com/hyperledger/chaincode/marbles02_private/collections_config.json --signature-policy "OR('Org1MSP.member','Org2MSP.member')" --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile $CORE_PEER_TLS_ROOTCERT_FILE
+peer lifecycle chaincode approveformyorg --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE --channelID $CHANNEL_NAME --name marbles02_private --version 1 --init-required --package-id $CC_PACKAGE_ID --sequence 1 --waitForEvent
 
 # 查看链码认证结果 此时Org1MSP和Org2MSP都审核通过了
 peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_NAME --name marbles02_private --version 1 --sequence 1 --output json --init-required
@@ -134,40 +132,23 @@ exit
 docker exec -it cli-org1-peer0 bash
 
 # 提交链码
-peer lifecycle chaincode commit -o orderer1.org0.example.com:7050 --channelID $CHANNEL_NAME --name marbles02_private --version 1 --sequence 1 --collections-config /opt/gopath/src/github.com/hyperledger/chaincode/marbles02_private/collections_config.json  --init-required --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE  --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE
+peer lifecycle chaincode commit -o orderer1.org0.example.com:7050 --channelID $CHANNEL_NAME --name marbles02_private --version 1 --sequence 1 --init-required --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE  --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE
 
 # 查询已经提交的链码
 peer lifecycle chaincode querycommitted --channelID $CHANNEL_NAME --name marbles02_private
 
+
 # 链码实例化 2.0版本以后取消了这步操作
 # peer chaincode instantiate -o orderer1.org0.example.com:7050 --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE -C mychannel -n marbles02_private -v 1.0 -c '{"Args":["Init","a","100","b","100"]}'
 
-export MARBLE=$(echo -n "{\"name\":\"marble1\",\"color\":\"blue\",\"size\":35,\"owner\":\"tom\",\"price\":99}" | base64 | tr -d \\n)
 # 链码执行
-peer chaincode invoke -o orderer1.org0.example.com:7050 --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE -C $CHANNEL_NAME -n marbles02_private --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE --isInit -c '{"Args":["InitMarble"]}' --transient "{\"marble\":\"$MARBLE\"}" --waitForEvent
+peer chaincode invoke -o orderer1.org0.example.com:7050 --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE -C $CHANNEL_NAME -n marbles02_private --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE --isInit -c '{"Args":["Init","a","100","b","100"]}' --waitForEvent
 
+# 链码数据查询
+peer chaincode query -n marbles02_private -C mychannel -c '{"Args":["query","a"]}'
 
-
-# Org1链码数据查询
-docker exec -it cli-org1-peer0 bash
-
-# Org1链码的私有数据
-peer chaincode query -n marbles02_private -C mychannel -c '{"Args":["ReadMarble","marble1"]}'
-
-# Org1链码的私有数据
-peer chaincode query -n marbles02_private -C mychannel -c '{"Args":["ReadMarblePrivateDetails","marble1"]}'
-
-
-
-# Org2链码数据查询
-docker exec -it cli-org2-peer0 bash
-
-# Org2链码的私有数据
-peer chaincode query -n marbles02_private -C mychannel -c '{"Args":["ReadMarble","marble1"]}'
-
-# Org2链码的私有数据
-peer chaincode query -n marbles02_private -C mychannel -c '{"Args":["ReadMarblePrivateDetails","marble1"]}'
-
+# 链码数据更新
+peer chaincode invoke -o orderer1.org0.example.com:7050 --tls true --cafile $CORE_PEER_TLS_ROOTCERT_FILE -C $CHANNEL_NAME -n marbles02_private --peerAddresses peer0.org1.example.com:7051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE --peerAddresses peer0.org2.example.com:9051 --tlsRootCertFiles $CORE_PEER_TLS_ROOTCERT_FILE -c '{"Args":["invoke","a","b","10"]}'  --waitForEvent
 
 
 
