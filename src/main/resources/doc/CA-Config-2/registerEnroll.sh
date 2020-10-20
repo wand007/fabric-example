af2245386b1730c9c4d0e0b129a50f749b8b5e38
李东东 <lidongdong@gxcards.com>
2020-10-20 14:33:41 +0800

    add

--- src/main/resources/doc/CA-Config-2/registerEnroll.sh
+++ src/main/resources/doc/CA-Config-2/registerEnroll.sh
@@ -649,6 +649,8 @@ configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts
 
 ##  docker-compose -f docker-compose-etcdraft2.yaml up -d 2>&1
 
-
+## curl -X PUT http://localhost:5984/_users
+## curl -X PUT http://localhost:5984/_replicator
+## curl -X PUT http://localhost:5984/fabric__internal
 
 
